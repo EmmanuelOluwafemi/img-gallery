f4d99a5bd5eb87afa2371271e46ffdeed5939e5b
EmmanuelOluwafemi <seek9ja@gmail.com>
2021-10-31 02:57:37 +0100

    added upadte

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import ReactPaginate from "react-paginate";
 
 function App() {
+  const [images, setImages] = useState([]);
+
+  const [currentImages, setCurrentImages] = useState(null);
+  const [pageCount, setPageCount] = useState(0);
+  const [imagesOffset, setImagesOffset] = useState(0);
+
+  useEffect(() => {
+    axios.get(`https://api.unsplash.com/photos/?client_id=${process.env.React_App_Client_ID}&per_page=30`)
+      .then(res => {
+        setImages(prevState => [...res.data]);
+        console.log(res.data);
+        return(res.data);
+      })
+      .catch(err => {
+          console.log(err);
+      })
+  }, [])
+
+
+  useEffect(() => {
+    // Fetch items from another resources.
+    const endOffset = imagesOffset+ 8;
+    setCurrentImages(images.slice(imagesOffset, endOffset));
+    setPageCount(Math.ceil(images.length / 8));
+  }, [images, imagesOffset]);
+
+
+  // Invoke when user click to request another page.
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * 8) % images.length;
+    setImagesOffset(newOffset);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h2>Imagery</h2>
+      <div className="App">
+        {
+          currentImages?.map((image, i) => {
+            return (
+              <div className="img-wrapper" key={i}>
+                <img src={image?.urls?.thumb} alt={image.alt_description} />
+              </div>
+            )
+          })
+        }
+      </div>
+
+      <div className="pagination">
+      <ReactPaginate
+        breakLabel="..."
+        nextLabel="next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={5}
+        pageCount={pageCount}
+        previousLabel="< previous"
+        renderOnZeroPageCount={null}
+        breakClassName={'page-item'}
+        breakLinkClassName={'page-link'}
+        containerClassName={'pagination'}
+        pageClassName={'page-item'}
+        pageLinkClassName={'page-link'}
+        previousClassName={'page-item'}
+        previousLinkClassName={'page-link'}
+        nextClassName={'page-item'}
+        nextLinkClassName={'page-link'}
+        activeClassName={'active'}       
+      />
+      </div>
+    </>
   );
 }
 
